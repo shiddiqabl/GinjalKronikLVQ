81f2e01fa2e44453bd81cf6a25ed55f40abf770c
shiddiqabl <shiddiq.abl@gmail.com>
2018-11-08 23:25:14 +0700

    Menambahkan fungsi normalisasi min max

--- application/models/Data_model.php
+++ application/models/Data_model.php
@@ -7,9 +7,9 @@ class Data_model extends CI_Model {
         
     }
     
-    function get_data() 
+    function get_data($table) 
     {
-        $query = $this->db->get('data_pasien');
+        $query = $this->db->get($table);
         if ($query->num_rows() > 0) 
         {
             return $query->result_array();
@@ -20,13 +20,37 @@ class Data_model extends CI_Model {
         }
     }
     
-    function insert_data($data) 
+    function get_data_raw($table)
     {
-        $this->db->insert('data_pasien', $data);
+        $query = $this->db->get($table);
+        if ($query->num_rows() > 0)
+        {
+            return $query;
+        }
+        else
+        {
+            return FALSE;
+        }
     }
     
-    function delete_data()
+    function get_data_alias($table)
     {
-        $this->db->empty_table('data_pasien');
+        $this->db->select("ID as '0', AGE as '1', BP as '2', SG as '3', AL as '4', SU as '5', RBC as '6', PC as '7', PCC as '8', 
+                            BA as '9', BGR as '10', BU as '11', SC as '12', SOD as '13', POT as '14', HEMO as '15', PCV as '16', 
+                            WBCC as '17', RBCC as '18', HTN as '19', DM as '20', CAD as '21', APPET as '22', PE as '23', ANE as '24', 
+                            CLASS as '25'");
+        $this->db->from($table);
+        $query = $this->db->get();
+        return $query;
+    }
+    
+    function insert_data($table, $data) 
+    {
+        $this->db->insert($table, $data);
+    }
+    
+    function delete_data($table)
+    {
+        $this->db->empty_table($table);
     }
 }
